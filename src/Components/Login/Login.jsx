088ef6ccafeccfddea256e82386a0d8d77d7916e
tarekul42 <tarekulrifat205@gmail.com>
2023-09-14 00:00:18 -0700

    Create Login page and recap validation

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password);
+
+        // password validation
+        setError('');
+        if(password.length < 6){
+            setError('Password must be at least 6 characters');
+            return;
+        }
+        else if(!/(?=.*?[A-Z])/.test(password)){
+            setError('Password must contain at least one number and one uppercase and lowercase letter');
+            return;
+        }
+
+
+    }
+
+
     return (
-        <div>
-            this is login page
+        <div className='w-25 mx-auto'>
+            <h1 className='text-success text-center'>Please Login</h1>
+            <Form onSubmit={handleLogin}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+                <p className='text-danger'>{error}</p>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accept Terms and Conditions" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
